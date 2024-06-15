a9b7f4c7891af9938c8a26d4451b228ec4057b62
gentaarya <mgentaarya@gmail.com>
2024-06-15 20:28:47 +0700

    fix jam

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -228,21 +228,21 @@ export const formatTimeAgo = (timestamp) => {
   const timeDifference = now - commentTime;
 
   const seconds = Math.floor(timeDifference / 1000);
-
   const minutes = Math.floor(seconds / 60);
-
   const hours = Math.floor(minutes / 60);
-
   const days = Math.floor(hours / 24);
-
+  const weeks = Math.floor(days / 7);
   const months = Math.floor(days / 30);
-
   const years = Math.floor(days / 365);
 
   if (years > 0) {
     return `${years} tahun yang lalu`;
   } else if (months > 0) {
     return `${months} bulan yang lalu`;
+  } else if (days >= 28) {
+    return `baru 1 bulan`;
+  } else if (weeks > 0) {
+    return `${weeks} minggu yang lalu`;
   } else if (days > 0) {
     return `${days} hari yang lalu`;
   } else if (hours > 0) {
